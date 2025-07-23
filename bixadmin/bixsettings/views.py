c36771c8ceec22740723d74cfc14ed57cc54427d
marcogarganigo <marcogarga11@gmail.com>
2025-07-23 16:58:19 +0200

    -

--- bixadmin/bixsettings/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-# Create your views here.
-# require admin role
-@login_required(login_url='/login/')
-def index(request):
-    return render(request, 'index.html')
