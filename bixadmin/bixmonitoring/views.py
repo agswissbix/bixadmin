5df7c2ebed42acf7e057bd6b62d69f958a5af320
Mark-Assi <110045452+marcogarganigo@users.noreply.github.com>
2025-07-25 16:53:50 +0200

    Add task success hook and improve scheduler integration
    Introduces a new hook system for Django Q tasks by adding a hook function in bixscheduler/hooks.py and registering it in settings.py. Updates scheduler views to set the hook on schedules, and implements output handling in bixmonitoring/views.py. Enhances task discovery in utils.py to include custom app scripts, and adjusts manage.py to include bixdata in PYTHONPATH. Minor refactoring and cleanup in tasks.py.

--- bixadmin/bixmonitoring/views.py
+++ bixadmin/bixmonitoring/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+def get_output(func, output, run_at):
+    print(f"[{run_at}] Funzione: {func} Output: {output}")
+    # Qui puoi poi salvare su DB o file, ora solo print
